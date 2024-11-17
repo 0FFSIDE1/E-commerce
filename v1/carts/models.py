d903ad16efca14b33bb655d01887cbcc02373cdc
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-17 22:09:26 +0100

    fix: cart table

--- v1/carts/models.py
+++ v1/carts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from customers.models import Customer
+from products.models import Product
 # Create your models here.
+class Cart(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, default=None, blank=True, null=True)
+    product = models.ManyToManyField(Product, default=None)
+    
+    def __str__(self):
+        return f"{self.product} | {self.customer}"
