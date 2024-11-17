48076a54a935c0e46e716e48837c8ca5a53975c6
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-17 23:23:41 +0100

    NEW FEAT: added custom middleware

--- v1/config/settings.py
+++ v1/config/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # custom middleware (check 'services/middleware')
+    'services.middleware.middleware.RequestMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
