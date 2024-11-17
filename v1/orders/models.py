d1abc3344af67720a31e3730605f7e76b4d05db1
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-17 22:10:34 +0100

    fix: order table

--- v1/orders/models.py
+++ v1/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from customers.models import Customer
+from products.models import Product
+import uuid
 # Create your models here.
+class Order(models.Model):
+    order_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
+    products = models.ManyToManyField(Product, default=None)
+    choices = (('Pending','Pending'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Canceled', 'Canceled'))
+    status = models.CharField(choices=choices, default='Pending', max_length=15)
+    total_price = models.CharField(max_length=20, default=None, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer
