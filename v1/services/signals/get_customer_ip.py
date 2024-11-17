0b844e7fa7d86d13670d6b43baf81226528f3932
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-17 23:24:48 +0100

    fix: signals/get_customer_ip

---
+++ v1/services/signals/get_customer_ip.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from customers.models import Customer
+from services.middleware.middleware import get_current_request
+
+
+@receiver(post_save, sender=Customer)
+def set_customer_ip(sender, instance, created, **kwargs):
+    if created and not instance.ip_address:
+        # Get the current request
+        request = get_current_request()
+        if request:
+            # Extract the IP address from the request
+            x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+            ip = x_forwarded_for.split(",")[0] if x_forwarded_for else request.META.get("REMOTE_ADDR")
+            instance.ip_address = ip
+            instance.save()
