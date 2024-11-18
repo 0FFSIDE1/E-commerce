f249e942a5d769b3e4e2bf0facdcaebae9128a83
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-18 15:17:21 +0100

    fix: edit bugs

--- v1/orders/models.py
+++ v1/orders/models.py
@@ -7,8 +7,8 @@ class Order(models.Model):
     order_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, default=None, related_name='cart', blank=False, null=False)
-    choices = (('Pending','Pending'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Canceled', 'Canceled'))
-    status = models.CharField(choices=choices, default='Pending', max_length=15)
+    choices = (('Created','Created'), ('Pending','Pending'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Canceled', 'Canceled'))
+    status = models.CharField(choices=choices, default='Created', max_length=15)
     total_price = models.CharField(max_length=20, default=None, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
