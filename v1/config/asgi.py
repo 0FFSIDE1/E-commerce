39310ebfc4d70690ae2b203a7e3ee0fb8263cd31
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-18 14:50:04 +0100

    fix: edits bugs, can create superuser "python manage.py createsuperuser --noinput"

--- v1/config/asgi.py
+++ v1/config/asgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import dotenv
+
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
+
+app = application
