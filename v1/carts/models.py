5253ffb6fee90ddaa36ed1554fa6930c57f88dfc
EzeXavier01 <ezexavier103@gmail.com>
2024-11-19 08:03:53 +0100

    new cart model

--- v1/carts/models.py
+++ v1/carts/models.py
@@ -4,7 +4,15 @@ from products.models import Product
 # Create your models here.
 class Cart(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, default=None, blank=True, null=True)
-    product = models.ManyToManyField(Product, default=None)
+    items = models.ManyToManyField(Product, through = "CartItem")
     
     def __str__(self):
         return f"{self.customer}"
+    
+class CartItem(models.Model):
+    cart =models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_item")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product.item_name}: ({self.quantity})"
