64a47790d64f76abbce634240ea4bd744a104847
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-21 00:20:04 +0100

    fix: bugs

--- v1/sellers/models.py
+++ v1/sellers/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class Vendor(models.Model):
     name = models.CharField(max_length=50, default=None, blank=False, null=False)
     address = models.CharField(max_length=225, default=None, blank=False, null=False)
-    email = models.EmailField(max_length=50, default=None, blank=False, null=False)
+    email = models.EmailField(max_length=50, default=None, blank=False, null=False, unique=True)
     phone = PhoneNumberField(_("Phone Number"), unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
