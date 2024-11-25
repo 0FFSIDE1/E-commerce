92c6a1f7ec08b0f86c45fd5a5436f2b3908fa72c
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-25 20:20:09 +0100

    fix: bugs

--- v1/orders/admin.py
+++ v1/orders/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order
 
 # Register your models here.
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
 
-class OrderAdmin(admin.ModelAdmin):
-    inlines = [
-        OrderItemInline
-    ]
-admin.site.register(Order, OrderAdmin)
+admin.site.register(Order)
