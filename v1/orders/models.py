4484449c73e41104e88a6a6693c9dab0bb3ec7bc
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-11-25 20:19:55 +0100

    fix: edit tables in order table

--- v1/orders/models.py
+++ v1/orders/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from customers.models import Customer
-from carts.models import Cart, CartItem
+from carts.models import Cart
 import uuid
-from products.models import Product
+from django.db.models import Sum
 # Create your models here.
 class Order(models.Model):
     order_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
@@ -10,23 +10,20 @@ class Order(models.Model):
     cart = models.OneToOneField(Cart, on_delete=models.CASCADE, default=None, related_name='cart', help_text="The cart used for this order")
     choices = (('Created','Created'), ('Pending','Pending'), ('Shipped', 'Shipped'), ('Delivered', 'Delivered'), ('Canceled', 'Canceled'))
     status = models.CharField(choices=choices, default='Created', max_length=15)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0, help_text="The total amount for the order")
+
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def total_amount(self):
-        # Calculate total amount from all items in the cart
-        return self.cart.items.aggregate(total=models.Sum('total_price'))['total'] or 0
+    def calculate_total_amount(self):
+        # Calculate the total amount for the order based on cart items
+        return self.cart.items.aggregate(total=Sum('total_price'))['total'] or 0
+
+    def save(self, *args, **kwargs):
+        if not self.total_amount:
+            self.total_amount = self.calculate_total_amount()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.customer.first_name} "
-    
+        return f"Order {self.order_id} by {self.customer.first_name}"
     
 
-
-    
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name='items', help_text="The order this item belongs to")
-    cartitems = models.ForeignKey(CartItem, on_delete=models.CASCADE, related_name='items', help_text="The cart item associated with this order item")
-
-  
-    def __str__(self):
-        return f"{self.cartitems.product.name} in Order {self.order.order_id}"
