a56ddccfd618dcc0ce07e238dfc88af10e469359
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-15 17:42:09 +0100

    fix

--- app/views.py
+++ app/views.py
@@ -5,9 +5,12 @@ from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.decorators import login_required, user_passes_test
+from services.utils.user import vendor_required
 
 # Create your views here.
 
-
+@login_required
+@user_passes_test(vendor_required, login_url='login', redirect_field_name='login')
 def index(request):
     return render(request, 'app/index.html')
