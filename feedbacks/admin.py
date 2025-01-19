068be99237280b0c565d7252234b081a98c8f7f7
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 09:34:12 +0100

    fix: customised admin

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import Feedback
 # Register your models here.
-admin.site.register(Feedback)
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('email', 'message')  # Columns in the list view
+    list_filter = ('created_at',)  # Filters on the sidebar
+    search_fields = ('email', )  # Searchable fields
+    ordering = ('created_at',)  # Default ordering
+    
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'message')
+        }),
+       
+    )
+
+   
