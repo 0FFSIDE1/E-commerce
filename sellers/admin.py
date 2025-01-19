68cacdf8e4f5b6333a38f64405e5fa6639dd0203
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 08:44:02 +0100

    fix: customised admin

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Vendor
 # Register your models here.
-admin.site.register(Vendor)
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'username', 'first_name', 'last_name', 'brand_name', 'phone', 'category') 
+    list_filter = ('created_at', )  
+    search_fields = ('email', 'username', 'brand_name', 'first_name', 'last_name')
+    ordering = ('id',)
+    readonly_fields = ('id', 'user' ,'currency')  
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'username', 'first_name', 'last_name', 'brand_name' , 'email', 'phone', 'address', 'city', 'state', 'country', 'category', 'brand_type', 'description', 'total_products', 'total_orders', 'total_orders_delivered', 'total_customers', 'total_reviews','currency')
+        }),
+        
+    )
