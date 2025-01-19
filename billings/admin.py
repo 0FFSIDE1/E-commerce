c08caa6174b0a081d40d59f1102414c9fef4bb52
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 08:42:52 +0100

    fix: customised admin

--- billings/admin.py
+++ billings/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Payment)
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'order', 'amount','payment_method', 'status',) 
+    list_filter = ('created_at', 'amount', 'status')  
+    search_fields = ('order', 'payment_id', 'amount', 'status')
+    ordering = ('payment_id',)
+    readonly_fields = ('order_id',)  
+    fieldsets = (
+        (None, {
+            'fields': ('order_id', 'order', 'amount', 'status', 'payment_method')
+        }),
+        
+    )
