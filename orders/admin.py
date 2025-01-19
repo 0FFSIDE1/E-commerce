5f37927a91ed5b8c0de75b013422a5ba1dc2baff
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 08:43:51 +0100

    fix: customised admin

--- orders/admin.py
+++ orders/admin.py
@@ -6,8 +6,20 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
+
+
+
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    inlines = [
-        OrderItemInline  
-    ]
-admin.site.register(Order, OrderAdmin)
+    list_display = ('order_id', 'customer', 'created_at', 'total_amount', 'status',) 
+    list_filter = ('created_at', 'status',)  
+    search_fields = ('order_id', 'customer', 'total_amount')
+    ordering = ('order_id',)
+    readonly_fields = ('order_id', 'total_amount', 'cart')  
+    fieldsets = (
+        (None, {
+            'fields': ('customer', 'cart', 'total_amount', 'status')
+        }),
+        
+    )
+    inlines = [OrderItemInline]
