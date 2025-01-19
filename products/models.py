20073b3e01903f239b6f6b640d012c36bc0f5e42
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 09:35:02 +0100

    fix: bugs

--- products/models.py
+++ products/models.py
@@ -60,6 +60,6 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.item_id}| {self.name} | {self.category} | {self.product_type}"
+        return f"{self.name}"
     
     
