0487f09405b454231e39435e14dae3172fe6d0f0
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-01-19 09:34:26 +0100

    fix: customised admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -7,8 +7,20 @@ class WishlistItemInline(admin.TabularInline):
     model = WishlistItem
     extra = 0
 
+
+
+
+@admin.register(Wishlist)
 class WishlistAdmin(admin.ModelAdmin):
-    inlines = [
-        WishlistItemInline  
-    ]
-admin.site.register(Wishlist, WishlistAdmin)
+    list_display = ('created_at', 'user', )  # Columns in the list view
+    list_filter = ('created_at',)  # Filters on the sidebar
+    search_fields = ('user', )  # Searchable fields
+    ordering = ('created_at',)  # Default ordering
+    
+    fieldsets = (
+        (None, {
+            'fields': ('user',)
+        }),
+       
+    )
+    inlines = [WishlistItemInline]
