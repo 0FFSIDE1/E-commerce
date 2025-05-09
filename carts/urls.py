2179804e47492e4d778fee1e0f116138c6e98419
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-09 11:04:53 +0100

    fix:

--- carts/urls.py
+++ carts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('api/v1/<str:pk>/add-to-cart', AddToCart, name="add-to-cart" ),
-    path('api/v1/get-cart', GetCart, name="get-cart" ),
-    path('api/v1/<int:pk>/update-cart', UpdateCartItem, name="update-cart" ),
-    path('api/v1/<int:pk>/delete', DeleteCartItem, name="delete-cartitem" ),
-
-
+    path('api/v1/add-to-cart', AddToCartView.as_view(), name="add-to-cart" ),
+    path('api/v1/get-cart', CartRetrieveView.as_view(), name="get-cart" ),
+    path('api/v1/<int:pk>/update-cart', CartItemUpdateView.as_view(), name="update-cart" ),
+    path('api/v1/<int:pk>/delete', CartItemDeleteView.as_view(), name="delete-cart" ),
 ]
